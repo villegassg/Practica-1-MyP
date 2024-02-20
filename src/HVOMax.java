79e3d7313297efc7c66184cba489745ca5c07bef
Luis Alberto Suarez Penaloza <112452521+LuisSuarez22@users.noreply.github.com>
2024-02-20 15:29:34 -0600

    Update HVOMax.java

--- src/HVOMax.java
+++ src/HVOMax.java
@@ -1,9 +1,9 @@
 import java.util.ArrayList;
 
 /**
- * Compañía de streaming HVOMax. Hereda de StreamingCompany.
+ * Compañía de streaming HVO Max. Hereda de StreamingCompany.
  */
-public class HVOMax extends StreamingCompany {
+public class HvoMax extends StreamingCompany {
 
     /**
      * Constructor único de una compañía de streaming. Le asigna un nombre y 
@@ -11,7 +11,7 @@ public class HVOMax extends StreamingCompany {
      * @param name el nombre de la compañía de streaming.
      * @param services la lista de servicios de la compañía de streaming.
      */
-    public HVOMax(String name, ArrayList<Service> services) {
+    public HvoMax(String name, ArrayList<Service> services) {
         super(name, services);
     }
     
@@ -19,30 +19,39 @@ public class HVOMax extends StreamingCompany {
      * Le notifica su pago mensual a todos los clientes (asumiendo que todos
      * contrataron el servicio al mismo tiempo).
      */
+    @Override
     public void notifyPayment() {
-
+        for (Client client : clients) {
+            System.out.println("Se ha cobrado el servicio de HVO Max a " + client.getName());
+        }
     }
 
     /**
      * Le notifica de su registro a la compañía de streaming al cliente que se 
      * acaba de suscribir.
      */
+    @Override
     public void notifyRegister(Client c) {
-
+        System.out.println("El cliente " + c.getName() + " se ha registrado en HVO Max.");
     }
 
     /**
      * Le notifica de su desuscripción a la compañía de streaming al cliente que se 
      * acaba de desuscribir.
      */
+    @Override
     public void notifyUnregister(Client c) {
-
+        System.out.println("El cliente " + c.getName() + " se ha desuscrito de HVO Max.");
     }
 
     /**
      * Le hace el cobro a todos los clientes de la compañía por sus servicios.
      */
+    @Override
     public void charge() {
-
+        for (Client client : clients) {
+            System.out.println("Cobro mensual de HVO Max a " + client.getName());
+            client.decreaseBalance(140); // Se cobra $140 por el servicio mensual de HVO Max
+        }
     }
 }
