567215d435dd1dbd0058a7e547a181c8cb85cbf3
Luis Alberto Suarez Penaloza <112452521+LuisSuarez22@users.noreply.github.com>
2024-02-20 15:33:11 -0600

    Update MomazonPrimeVideo.java

--- src/MomazonPrimeVideo.java
+++ src/MomazonPrimeVideo.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 /**
- * Compañía de streaming MomazonPrimeVideo. Hereda de StreamingCompany.
+ * Compañía de streaming Momazon Prime Video. Hereda de StreamingCompany.
  */
 public class MomazonPrimeVideo extends StreamingCompany {
     
@@ -19,30 +19,39 @@ public class MomazonPrimeVideo extends StreamingCompany {
      * Le notifica su pago mensual a todos los clientes (asumiendo que todos
      * contrataron el servicio al mismo tiempo).
      */
+    @Override
     public void notifyPayment() {
-
+        for (Client client : clients) {
+            System.out.println("Se ha cobrado el servicio de Momazon Prime Video a " + client.getName());
+        }
     }
 
     /**
      * Le notifica de su registro a la compañía de streaming al cliente que se 
      * acaba de suscribir.
      */
+    @Override
     public void notifyRegister(Client c) {
-
+        System.out.println("El cliente " + c.getName() + " se ha registrado en Momazon Prime Video.");
     }
 
     /**
      * Le notifica de su desuscripción a la compañía de streaming al cliente que se 
      * acaba de desuscribir.
      */
+    @Override
     public void notifyUnregister(Client c) {
-
+        System.out.println("El cliente " + c.getName() + " se ha desuscrito de Momazon Prime Video.");
     }
 
     /**
      * Le hace el cobro a todos los clientes de la compañía por sus servicios.
      */
+    @Override
     public void charge() {
-
+        for (Client client : clients) {
+            System.out.println("Cobro mensual de Momazon Prime Video a " + client.getName());
+            client.decreaseBalance(150); // Se cobra $150 por el servicio mensual de Momazon Prime Video
+        }
     }
 }
