3b5ba6b5eb422a0469a86ba03d812545679b3b2d
Luis Alberto Suarez Penaloza <112452521+LuisSuarez22@users.noreply.github.com>
2024-02-20 15:25:54 -0600

    Update Spootify.java

--- src/Spootify.java
+++ src/Spootify.java
@@ -19,30 +19,39 @@ public class Spootify extends StreamingCompany {
      * Le notifica su pago mensual a todos los clientes (asumiendo que todos
      * contrataron el servicio al mismo tiempo).
      */
+    @Override
     public void notifyPayment() {
-
+        for (Client client : clients) {
+            System.out.println("Se ha cobrado el servicio de Spootify a " + client.getName());
+        }
     }
 
     /**
      * Le notifica de su registro a la compañía de streaming al cliente que se 
      * acaba de suscribir.
      */
+    @Override
     public void notifyRegister(Client c) {
-
+        System.out.println("El cliente " + c.getName() + " se ha registrado en Spootify.");
     }
 
     /**
      * Le notifica de su desuscripción a la compañía de streaming al cliente que se 
      * acaba de desuscribir.
      */
+    @Override
     public void notifyUnregister(Client c) {
-
+        System.out.println("El cliente " + c.getName() + " se ha desuscrito de Spootify.");
     }
 
     /**
      * Le hace el cobro a todos los clientes de la compañía por sus servicios.
      */
+    @Override
     public void charge() {
-
+        for (Client client : clients) {
+            System.out.println("Cobro mensual de Spootify a " + client.getName());
+            client.decreaseBalance(80); // Se cobra $80 por el servicio mensual de Spootify
+        }
     }
 }
